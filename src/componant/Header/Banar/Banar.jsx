77503f122ecacdb654b7b80d1aa8d4a85f2474f2
hridaysonar <mdhridaysonar@gmail.com>
2025-04-16 22:55:09 +0600

    book data done

--- src/componant/Header/Banar/Banar.jsx
+++ src/componant/Header/Banar/Banar.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import bookimge from '../../../assets/books.jpg'
 const Banar = () => {
     return (
-        <div>
-            <div className=' flex justify-around items-center p-16 w-full'>
-                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore accusamus, fugit omnis, vero praesentium hic natus tempore corporis voluptatibus accusantium repellendus ut unde fuga deleniti voluptatem officiis commodi culpa. Molestiae!</h1>
-                <button className=' btn-primary'>test test</button>
-            </div>
-            <div>
-                <img className='w-3/12' src={bookimge} alt="" />
-            </div>
+        <div className="flex justify-between items-center px-20 py-16 w-full">
+        <div className="flex flex-col items-start gap-6 max-w-lg">
+          <h1 className="text-4xl font-bold leading-snug text-gray-900">
+            Books to freshen up<br />your bookshelf
+          </h1>
+          <button className="bg-green-500 text-white font-semibold px-6 py-3 rounded-md shadow hover:bg-green-600 transition cursor-pointer ">
+            View The List
+          </button>
         </div>
+        <div>
+          <img className="w-48 md:w-60 lg:w-72" src={bookimge} alt="Book cover" />
+        </div>
+      </div>
+      
+
     );
 };
 
