d0598d2bc52d7249b24ebc7582f24cb8ebf59899
hridaysonar <mdhridaysonar@gmail.com>
2025-04-17 11:20:54 +0600

    card stard

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -11,7 +11,7 @@ const Book = () => {
         <div>
             
             <h1 className='text-3xl text-center p-6'>Books</h1>
-           <div className=' grid grid-cols-3' >
+           <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4' >
            {
                 getBooks.map(book =><Singel key={book.bookId} book={book}></Singel>)
             }
--- src/pages/Book/Singel.jsx
+++ src/pages/Book/Singel.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 
-const Singel = ({book}) => {
+const Singel = ({ book }) => {
     return (
-        <div>
-            <div className="card bg-base-100 w-96 shadow-sm">
-                <figure className="px-2 pt-2">
-                    <img
-                        src={book.image}
-                        alt="Shoes"
-                        className="rounded-xl" />
-                </figure>
-                <div className="card-body items-center text-center">
-                    <h2 className="card-title">{book.bookName
-                    }</h2>
-                    <p>By : {book.author}</p>
-                    
-                    <div className="card-actions border-dashed ">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
+        <div className="card bg-base-100 w-80 shadow-md border">
+            <figure className="px-4 pt-4">
+                <img src={book.image} alt={book.bookName} className="rounded-xl h-52 object-contain" />
+            </figure>
+            <div className="card-body items-center text-center space-y-2">
+                <div className="flex flex-wrap justify-center gap-1">
+                    {book.tags.map((tag, index) => (
+                        <span
+                            key={index}
+                            className="text-xs px-2 py-1 bg-green-100 text-green-600 rounded-full"
+                        >
+                            {tag}
+                        </span>
+                    ))}
+                </div>
+                <h2 className="card-title text-lg">{book.bookName}</h2>
+                <p className="text-sm text-gray-600">By : {book.author}</p>
+                <p className="text-sm font-light">{book.category}</p>
+                <div className="flex items-center justify-between w-full">
+                    <p className="text-sm">Rating: {book.rating}</p>
+                    <button className="btn btn-sm btn-primary">Buy Now</button>
                 </div>
             </div>
         </div>
